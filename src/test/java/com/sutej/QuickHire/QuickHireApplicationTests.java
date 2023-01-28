35aa0c4d126d588534544f6613c8f5350a100a47
Sutejsk28 <70704957+Sutejsk28@users.noreply.github.com>
2023-01-29 00:57:24 +0530

    create spring boot project

---
+++ src/test/java/com/sutej/QuickHire/QuickHireApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sutej.QuickHire;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickHireApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
